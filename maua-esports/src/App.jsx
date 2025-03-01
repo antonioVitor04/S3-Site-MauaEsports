1d2d750ca051b8a6788a1f2601e0de1fdc322eeb
alvaro <alvarojunqueira881@gmail.com>
2025-02-28 21:12:50 -0300

    first commit

---
+++ maua-esports/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-400">
+      <h1 className="text-white bg-slate-400 hover:bg-slate-700">ola</h1>
+    </div>
+  );
+}
+
+export default App;
