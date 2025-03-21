81ac2b7d37a263ae9e0abbacea3dc8aaec8a3413
alvaro <alvarojunqueira881@gmail.com>
2025-03-21 02:29:37 -0300

    aprimorando navbar

--- maua-esports/src/components/NavBar.tsx
+++ maua-esports/src/components/NavBar.tsx
@@ -1,44 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"; // Para navegação
-import { IoMdArrowDropdown, IoMdClose } from "react-icons/io";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useAuth } from "../contexts/AuthContexts"; // Importando o hook useAuth para acessar o contexto
-import LogoutBtn from "./LogoutBtn";
-import { CgLogIn } from "react-icons/cg";
-import logo from "../assets/images/Logo.svg"; // Importa o arquivo SVG
+import { Link } from "react-router-dom"; // Para navegação entre páginas
+import { IoMdArrowDropdown, IoMdClose } from "react-icons/io"; // Ícones de dropdown e fechar
+import { GiHamburgerMenu } from "react-icons/gi"; // Ícone do menu hambúrguer
+import { useAuth } from "../contexts/AuthContexts"; // Hook para autenticação
+import { CgLogIn, CgLogOut } from "react-icons/cg"; // Ícones de login e logout
+import { RiTeamFill } from "react-icons/ri"; // Ícone de times
+import { FaUserTie, FaRegClock } from "react-icons/fa"; // Ícones de usuário e relógio
+import { HiUserCircle } from "react-icons/hi2"; // Ícone de perfil do usuário
+import logo from "../assets/images/Logo.svg"; // Logo da aplicação
 
 const NavBar = () => {
-  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false); // Controla o estado do menu hambúrguer
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Controla o estado do dropdown
+  // Estados para controlar o menu hambúrguer, dropdowns e scroll
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false); // Controla a abertura do menu hambúrguer
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // Controla a abertura do dropdown de times
   const [isScrolled, setIsScrolled] = useState(false); // Verifica se a página foi rolada
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false); // Controla a abertura do dropdown do perfil
 
-  // Consumindo o estado de login e as funções do contexto de autenticação
+  // Consumindo o contexto de autenticação
   const { isLoggedIn, fazerLogin, fazerLogout } = useAuth();
 
+  // Efeito para detectar scroll da página
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50);
+      setIsScrolled(window.scrollY > 50); // Atualiza o estado se o scroll for maior que 50px
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll); // Adiciona o listener de scroll
+    return () => window.removeEventListener("scroll", handleScroll); // Remove o listener ao desmontar
   }, []);
 
-  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen); // Controla o dropdown
-  const toggleHamburgerMenu = () => setIsHamburgerOpen(!isHamburgerOpen); // Controla o menu hambúrguer
+  // Funções para alternar estados
+  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen); // Alterna o dropdown de times
+  const toggleProfileDropdown = () =>
+    setIsProfileDropdownOpen(!isProfileDropdownOpen); // Alterna o dropdown do perfil
+  const toggleHamburgerMenu = () => setIsHamburgerOpen(!isHamburgerOpen); // Alterna o menu hambúrguer
 
   return (
     <nav
       className={`py-5 fixed w-full flex justify-between items-center text-lg text-white font-blinker transition-all duration-300 ease-in-out ${
-        isScrolled ? "bg-preto bg-opacity-70" : "bg-preto lg:bg-transparent"
+        isScrolled ? "bg-navbar bg-opacity-70" : "bg-navbar lg:bg-transparent"
       }`}
     >
-      {/* Logo */}
-      <div className="mx-8">
+      {/* Borda animada que aparece ao rolar a página */}
+      <div className="absolute bottom-0 left-0 w-full h-[2px] bg-transparent">
+        <div
+          className={`absolute left-0 h-full bg-borda transition-all duration-500 ${
+            isScrolled ? "w-full" : "w-0"
+          }`}
+        />
+      </div>
+
+      {/* Logo da aplicação */}
+      <div className="mx-14">
         <img className="w-16 h-16" src={logo} alt="Logo" />
       </div>
 
-      {/* Botão do menu hambúrguer (visível em telas pequenas) */}
+      {/* Botão do menu hambúrguer (visível apenas em telas pequenas) */}
       <button
         className={`lg:hidden text-white fixed right-5 cursor-pointer transform transition-all duration-300 ${
           isHamburgerOpen ? "rotate-90" : "rotate-0"
@@ -46,9 +63,9 @@ const NavBar = () => {
         onClick={toggleHamburgerMenu}
       >
         {isHamburgerOpen ? (
-          <IoMdClose className="text-2xl" />
+          <IoMdClose className="text-2xl" /> // Ícone de fechar
         ) : (
-          <GiHamburgerMenu className="text-2xl" />
+          <GiHamburgerMenu className="text-2xl" /> // Ícone do menu hambúrguer
         )}
       </button>
 
@@ -57,123 +74,178 @@ const NavBar = () => {
         className={`gap-6 items-center lg:flex ${
           isHamburgerOpen
             ? "mx-0 bg-preto flex flex-col w-full absolute top-full left-0 justify-center items-center"
-            : "lg:block hidden mx-25"
+            : "lg:block hidden mx-14"
         }`}
       >
+        {/* Item "Sobre" */}
         <li
-          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] 
-  border-b-2 ${
-    isHamburgerOpen
-      ? "border-white"
-      : "border-b-transparent hover:border-gray-300"
-  }`}
+          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] border-b-3 ${
+            isHamburgerOpen
+              ? "border-borda"
+              : "border-b-transparent hover:border-borda rounded-md"
+          }`}
         >
           <Link to="/">Sobre</Link>
         </li>
 
+        {/* Item "Times" com dropdown */}
         <li
-          className={`relative px-4 py-2 transition-transform duration-300 hover:translate-y-[-4px] 
-  border-b-2 ${
-    isHamburgerOpen
-      ? "border-white"
-      : "border-b-transparent hover:border-gray-300"
-  }`}
+          className={`relative px-4 py-2 transition-transform duration-300 hover:translate-y-[-4px] border-b-3 ${
+            isHamburgerOpen
+              ? "border-borda"
+              : "border-b-transparent hover:border-borda rounded-md"
+          }`}
         >
           <button
             onClick={toggleDropdown}
-            className="focus:outline-none flex items-center gap-1 cursor-pointer "
+            className="focus:outline-none flex items-center gap-1 cursor-pointer"
           >
             Times{" "}
             <IoMdArrowDropdown
               className={`transition-transform duration-300 ${
                 isDropdownOpen ? "rotate-180" : "rotate-0"
-              }`}
+              }`} // Animação de rotação do ícone
             />
           </button>
 
-          {/* Dropdown */}
+          {/* Dropdown de times */}
           <ul
-            className={`absolute bg-preto shadow-lg py-2 rounded-lg 
-    transition-all duration-300 ease-out text-center
-    ${
-      // Verifica se o dropdown está aberto
-      isDropdownOpen
-        ? "opacity-100 translate-y-0 visible"
-        : "opacity-0 translate-y-[-20px] invisible"
-    }  
-    ${
-      // Verifica se o hambúrguer está aberto, para ocupar 100% de largura
-      isHamburgerOpen && isDropdownOpen
-        ? "w-full border-t-2 rounded-none mt-4 relative"
-        : "mt-12"
-    }`}
+            className={`absolute bg-fundo border-2 border-borda shadow-lg rounded-lg transition-all duration-300 ease-out text-center ${
+              isDropdownOpen
+                ? "opacity-100 translate-y-0 visible"
+                : "opacity-0 translate-y-[-20px] invisible"
+            } ${
+              isHamburgerOpen && isDropdownOpen
+                ? "w-full border-t-2 rounded-none mt-4 relative right-0"
+                : "mt-12 right-[-60px]"
+            }`}
           >
-            <li className="px-4 py-2 border-b-2 relative">
-              {/* Esconde a seta quando hambúrguer está aberto */}
-              {!isHamburgerOpen && (
-                <span className="absolute left-1/2 transform -translate-x-1/2 text-4xl rotate-180 top-[-30px] text-black">
-                  <IoMdArrowDropdown />
-                </span>
-              )}
-              <span className="inline-block transform hover:scale-110 transition-transform duration-300 cursor-pointer">
-                <Link to="/times">Membros</Link>
-              </span>
+            <li className="px-4 py-2 border-b-2 border-borda relative">
+              <Link
+                to="/times"
+                className="px-4 bg-transparent border-transparent inline-flex items-center gap-2 transform hover:scale-110 transition-transform duration-300 cursor-pointer hover:bg-hover hover:border-2 hover:border-borda hover:rounded-md"
+              >
+                <RiTeamFill />
+                Membros
+              </Link>
             </li>
+
             <li className="px-4 py-2">
-              <span className="inline-block transform hover:scale-110 transition-transform duration-300 cursor-pointer ">
-                <Link to="/staff">Staff</Link>
-              </span>
+              <Link
+                to="/times"
+                className="px-4 bg-transparent border-transparent inline-flex items-center gap-2 transform hover:scale-110 transition-transform duration-300 hover:bg-hover hover:border-2 hover:border-borda hover:rounded-md cursor-pointer"
+              >
+                <FaUserTie />
+                Administradores
+              </Link>
             </li>
           </ul>
         </li>
 
+        {/* Item "Campeonatos" */}
         <li
-          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] 
-            border-b-2 ${
-              isHamburgerOpen
-                ? "border-white"
-                : "border-b-transparent hover:border-gray-300"
-            }`}
+          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] border-b-3 ${
+            isHamburgerOpen
+              ? "border-borda"
+              : "border-b-transparent hover:border-borda rounded-md"
+          }`}
         >
           <Link to="/campeonatos">Campeonatos</Link>
         </li>
+
+        {/* Item "Novidades" */}
         <li
-          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] 
-    border-b-2 ${
-      isHamburgerOpen
-        ? "border-white"
-        : "border-b-transparent hover:border-gray-300"
-    }`}
+          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] border-b-3 ${
+            isHamburgerOpen
+              ? "border-borda"
+              : "border-b-transparent hover:border-borda rounded-md"
+          }`}
         >
           <Link to="/novidades">Novidades</Link>
         </li>
+
+        {/* Ícone do usuário e dropdown do perfil */}
         <li
-          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] 
-  border-b-2 ${
-    isLoggedIn
-      ? isHamburgerOpen
-        ? "border-white"
-        : "border-b-transparent hover:border-gray-300"
-      : "hidden"
-  }`}
+          className={`px-4 py-2 cursor-pointer transition-transform duration-300 hover:translate-y-[-4px] ${
+            !isLoggedIn ? "hidden" : "block"
+          } relative`}
         >
-          <Link to="/horas-paes">Horas PAEs</Link>
+          <button
+            onClick={toggleProfileDropdown}
+            className="flex justify-center items-center border-2 rounded-full border-borda w-10 h-10 cursor-pointer"
+          >
+            <HiUserCircle className="w-full h-full" /> {/* Ícone do perfil */}
+          </button>
+
+          {/* Dropdown do perfil */}
+          <ul
+            className={`absolute left-1/2 transform -translate-x-1/2 transition-all duration-300 ease-out text-center ${
+              isProfileDropdownOpen
+                ? "opacity-100 translate-y-0 visible"
+                : "opacity-0 translate-y-[-20px] invisible"
+            } ${
+              isHamburgerOpen && isProfileDropdownOpen
+                ? "w-full border-t-2 rounded-none mt-24"
+                : "mt-12"
+            }`}
+          >
+            <div className="bg-fundo w-70 h-90 border-2 border-borda shadow-azul-claro shadow-sm rounded-lg flex flex-col">
+              {/* Cabeçalho com ícone, nome e email */}
+              <div className="w-full h-20 flex border-b-2 border-borda items-center p-4 gap-3">
+                <div className="w-12 h-12">
+                  <HiUserCircle className="w-full h-full" />{" "}
+                  {/* Ícone do perfil */}
+                </div>
+                <div className="flex flex-col flex-grow items-start overflow-hidden">
+                  <h1 className="font-bold">Usuário</h1>
+                  <p className="text-sm whitespace-nowrap overflow-hidden text-ellipsis">
+                    24.01402-8@maua.br
+                  </p>
+                </div>
+                <button className="bg-hover rounded-md px-2 py-1 font-bold text-sm cursor-pointer">
+                  Editar
+                </button>
+              </div>
+
+              {/* Opções do menu */}
+              <div className="flex-grow">
+                <div className="w-full p-2 cursor-pointer flex items-center gap-3 hover:bg-hover">
+                  <div className="w-10 h-10 flex items-center justify-center">
+                    <FaRegClock className="text-2xl text-azul-claro" />{" "}
+                    {/* Ícone de relógio */}
+                  </div>
+                  <div className="flex flex-col flex-grow items-start overflow-hidden">
+                    <h1 className="font-bold">Horas PAEs</h1>
+                    <p className="text-sm whitespace-nowrap overflow-hidden text-ellipsis">
+                      Consulte suas horas PAEs
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Botão de Sair da conta */}
+              <button
+                className="w-full border-t-2 border-borda p-2 mt-auto text-vermelho-claro flex items-center gap-2 cursor-pointer hover:bg-hover"
+                onClick={fazerLogout}
+              >
+                <CgLogOut className="text-2xl" /> {/* Ícone de logout */}
+                <span>Sair da conta</span>
+              </button>
+            </div>
+          </ul>
         </li>
-        {/* Exibindo o botão de login, que chama a função de login */}
+
+        {/* Botão de Login (exibido apenas se o usuário não estiver logado) */}
         <li className="px-4 py-2 cursor-pointer">
-          {!isLoggedIn ? (
+          {!isLoggedIn && (
             <button
               onClick={fazerLogin}
-              className="relative flex items-center justify-center px-4 py-2 gap-2 border border-white text-white rounded-md overflow-hidden transition-all duration-300 cursor-pointer 
-                       before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-azul-claro
-                       before:transition-all before:duration-500 hover:before:w-full"
+              className="relative flex items-center justify-center px-4 py-2 gap-2 border-2 border-borda text-white rounded-md overflow-hidden transition-all duration-300 cursor-pointer before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-azul-claro before:transition-all before:duration-500 hover:before:w-full"
             >
               <span className="relative z-10 flex items-center gap-2">
-                Login <CgLogIn />
+                Login <CgLogIn /> {/* Ícone de login */}
               </span>
             </button>
-          ) : (
-            <LogoutBtn fazerLogout={fazerLogout} />
           )}
         </li>
       </ul>
