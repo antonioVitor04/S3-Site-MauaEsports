aaf042fda9e7d1e3b4f819974427057839841458
antonioVitor04 <antonionapoli394@gmail.com>
2025-05-07 19:45:30 -0300

    atualizações

--- maua-esports/src/components/PageBanner.jsx
+++ maua-esports/src/components/PageBanner.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const PageBanner = ({ pageName }) => {
     return (
-        <h1 className="h-24 flex items-center justify-center text-xl sm:text-1xl font-bold bg-[#010409] text-white">
+        <h1 className="h-24 flex items-center justify-center text-3xl sm:text-3xl font-bold bg-[#010409] text-white pb-10">
             {pageName}
         </h1>
     );
